cf8c4467ea437b565eb3f5570d2ad6760e03eb1a
solid-eureka <119814169+solid-eureka@users.noreply.github.com>
2025-05-25 10:53:53 +0000

    Automatically benchmark all mitigations

--- kernel_modules/benchmarks/run-all.sh
+++ kernel_modules/benchmarks/run-all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd -- "$(dirname -- "${BASH_SOURCE:-$0}")"
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root (use sudo)" 
+    exit 1
+fi
+
 echo "Testing baseline"
 echo "Removing all modules"
 sudo rmmod clear_icache_on_context_switch
@@ -9,31 +14,32 @@ sudo rmmod csr_emulation
 sudo rmmod fuzzy_timing_sigill
 echo "Current modules:"
 lsmod
+echo "-------------------------------------"
 ./run-current.sh "$1" # pass argument along ("nomake")
 
 # move results (use sudo, as some results are owned by root)
 OUT_DIR="results/baseline/"
 mkdir -p "$OUT_DIR"
-sudo mv *.stat "$OUT_DIR"
-sudo mv *.log  "$OUT_DIR"
+sudo mv -f *.stat "$OUT_DIR"
+sudo mv -f *.log  "$OUT_DIR"
 
 
 
 echo "Testing mitigations"
-sudo modprobe ./../fuzzy_timing_sigill/fuzzy_timing_sigill.ko
+sudo insmod ./../fuzzy_timing_sigill/fuzzy_timing_sigill.ko
 ./run-current.sh "$1" # pass argument along ("nomake")
 sudo rmmod fuzzy_timing_sigill
 
 # move results (use sudo, as some results are owned by root)
 OUT_DIR="results/catch_wait/"
 mkdir -p "$OUT_DIR"
-sudo mv *.stat "$OUT_DIR"
-sudo mv *.log  "$OUT_DIR"
+sudo mv -f *.stat "$OUT_DIR"
+sudo mv -f *.log  "$OUT_DIR"
 
 
 
-sudo modprobe ./../csr_emulation/csr_emulation.ko
-sudo modprobe ./../disable_csr/disable_csr.ko
+sudo insmod ./../csr_emulation/csr_emulation.ko
+sudo insmod ./../disable_csr/disable_csr.ko
 ./run-current.sh "$1"
 sudo rmmod disable_csr
 sudo rmmod csr_emulation
@@ -41,9 +47,17 @@ sudo rmmod csr_emulation
 # move results
 OUT_DIR="results/disable_emulate/"
 mkdir -p "$OUT_DIR"
-sudo mv *.stat "$OUT_DIR"
-sudo mv *.log  "$OUT_DIR"
+sudo mv -f *.stat "$OUT_DIR"
+sudo mv -f *.log  "$OUT_DIR"
 
 
 
-# TODO
+sudo insmod ./../clear_icache_on_context_switch/clear_icache_on_context_switch.ko
+./run-current.sh "$1"
+sudo rmmod clear_icache_on_context_switch
+
+# move results
+OUT_DIR="results/switch_clear/"
+mkdir -p "$OUT_DIR"
+sudo mv -f *.stat "$OUT_DIR"
+sudo mv -f *.log  "$OUT_DIR"
