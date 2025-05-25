2a252d7035061f6d2866e966c780d6043983957d
Petzys <87223648+Petzys@users.noreply.github.com>
2025-05-25 10:02:55 +0000

    add boilerplate to context_switches run.sh

--- kernel_modules/benchmarks/context_switches/run.sh
+++ kernel_modules/benchmarks/context_switches/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+cd -- "$(dirname -- "${BASH_SOURCE:-$0}")"
+
+echo "Running context_switches"
 sudo perf stat -o ../context_switches.stat stress-ng --switch 8 --timeout 60s --metrics --log-file ../context_switches.log
